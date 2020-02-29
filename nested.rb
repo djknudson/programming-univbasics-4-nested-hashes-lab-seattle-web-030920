9b9c5d2ab90a5ebb5100c38905a50bbceffb7dc4
David Knudson <djknudson@icloud.com>
2020-02-29 09:43:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,11 +76,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
+
 		 programmer_hash[:yukihiro_matsumoto] = {
 		   :known_for => "Ruby",
 		    :languages => ["LISP", "C"]
 		 }
-
+		 return programmer_hash[:yukihiro_matsumoto]
 end
 
 def changing_alan
